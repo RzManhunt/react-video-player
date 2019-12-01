469ad92466d133df38631acceb5036ef625997ec
Luis <lamsarmiento@gmail.com>
2019-12-01 01:17:39 -0400

    Updates

---
+++ precache-manifest.ec8ae7dcda4a976c8c1587d3eb978559.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907b313cc0fda6e499d3c1591bebbe3d",
+    "url": "/react-video-player/index.html"
+  },
+  {
+    "revision": "a15c2c8a51e32987ce4e",
+    "url": "/react-video-player/static/js/2.6dd18f50.chunk.js"
+  },
+  {
+    "revision": "0bfe108f6d570caf082a",
+    "url": "/react-video-player/static/js/main.4c0daa62.chunk.js"
+  },
+  {
+    "revision": "d8aa0c30abdc9cf8b692",
+    "url": "/react-video-player/static/js/runtime-main.87e3b297.js"
+  }
+]);
